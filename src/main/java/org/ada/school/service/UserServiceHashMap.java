19e68b3e6836d3da1f471ff69b8cb366aedd6d95
Sebastian Herrera <sebastianhh1999@gmail.com>
2021-09-01 10:34:38 -0500

    Service with MongoDB

--- src/main/java/org/ada/school/service/UserServiceHashMap.java
+++ src/main/java/org/ada/school/service/UserServiceHashMap.java
@@ -1,54 +1,54 @@
 package org.ada.school.service;
 
 import org.ada.school.dto.UserDto;
-import org.ada.school.model.User;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import org.ada.school.repository.UserDocument;
+
 import java.util.List;
 
-@Service
 public class UserServiceHashMap
     implements UserService
 {
 
-    private final HashMap<String, User> usersMap = new HashMap<>();
+//    private final HashMap<String, User> usersMap = new HashMap<>();
 
 
     @Override
-    public User create( User user )
+    public UserDocument create( UserDto user )
     {
-        usersMap.put( user.getId(), user );
-        return user;
-    }
-
-    @Override
-    public User findById( String id )
-    {
-        if ( usersMap.containsKey( id ) )
-        {
-            return usersMap.get( id );
-        }
+        //usersMap.put( user.getId(), user );
+        //return user;
         return null;
     }
 
     @Override
-    public List<User> all()
+    public UserDocument findById( String id )
     {
-        return new ArrayList<>( usersMap.values() );
+        /*if ( usersMap.containsKey( id ) )
+        {
+            return usersMap.get( id );
+        }*/
+        return null;
+    }
+
+    @Override
+    public List<UserDocument> all()
+    {
+        return null;
+        //return new ArrayList<>( usersMap.values() );
     }
 
     @Override
     public boolean deleteById( String id )
     {
-        return usersMap.remove( id ) != null;
+        //return usersMap.remove( id ) != null;
+        return false;
     }
 
     @Override
-    public User update( UserDto userDto, String id )
+    public UserDocument update( UserDto userDto, String id )
     {
-        if ( usersMap.containsKey( id ) )
+        /*if ( usersMap.containsKey( id ) )
         {
             User user = usersMap.get( id );
             user.update( userDto );
@@ -57,7 +57,8 @@ public class UserServiceHashMap
         else
         {
             return null;
-        }
+        }*/
+        return null;
     }
 
 
